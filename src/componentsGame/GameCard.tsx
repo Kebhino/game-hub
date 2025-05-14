ae5f014f74f9a2434f7c7ded471a5ed1ef68cf7b
Kebhino <wrobeljakub@gmail.com>
2025-05-14 11:39:35 +0200

    Clean up the game card

--- src/componentsGame/GameCard.tsx
+++ src/componentsGame/GameCard.tsx
@@ -14,13 +14,13 @@ const GameCard = ({ game }: Props) => {
     <CardRoot size="md">
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <Card.Body>
-        <CardHeader fontSize="lg">{game.name}</CardHeader>
         <HStack justifyContent={"space-between"}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <CardHeader fontSize="lg">{game.name}</CardHeader>
       </Card.Body>
     </CardRoot>
   );
--- src/componentsGame/PlatformIconList.tsx
+++ src/componentsGame/PlatformIconList.tsx
@@ -29,7 +29,7 @@ const PlatformIconList = ({ platforms }: Props) => {
     web: BsGlobe,
   };
   return (
-    <HStack marginY={3}>
+    <HStack marginY={1}>
       {platforms.map((platform) => (
         <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
       ))}
